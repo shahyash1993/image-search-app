f1d06285d4e4ade9bc35c8d0fdc9906bf80a8edf
Yash Paragkumar Shah <ypshah.13@gmail.com>
2019-02-15 16:27:04 -0800

    ImageCard introduced

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -2,13 +2,19 @@ import React, { Component } from "react";
 
 class ImageCard extends Component {
   state = {};
+
+  //in constructor, 'props'; but in render, 'this.props'
+  constructor(props) {
+    super(props);
+    this.imageRef = React.createRef;
+  }
+
   render() {
+    const { urls, description } = this.props.image;
+
     return (
       <div>
-        <img
-          src={this.props.image.urls.small}
-          alt={this.props.image.description}
-        />
+        <img ref={this.imageRef} src={urls.small} alt={description} />
       </div>
     );
   }
--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
+import ImageCard from "./ImageCard.jsx";
+
 import "./ImageList.css";
 
 const ImageList = props => {
   console.log("props.images=", props.images);
 
   //in list only the root elements to be returned has the prop 'key'
-  const images = props.images.map(({ id, description, urls }) => (
-    <div key={id}>
-      <img src={urls.small} alt={description} />
+  const images = props.images.map(image => (
+    <div key={image.id}>
+      {/* when there was destructuring. */}
+      {/* <img src={urls.small} alt={description} /> */}
+      <ImageCard image={image} />
     </div>
   ));
 
